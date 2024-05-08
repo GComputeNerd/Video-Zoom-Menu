1cea4a9eed276a33f90b08af25e5ebf64f031e6e
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-09 02:52:10 +0800

    Zoom In function implemented

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ def zoomFrame(clip, result,cropped, center):
     
     return n_clip.resize(result)
 
+def zoomIn(clip, ogSize, croppedSize, center):
+    w = ogSize[0]
+    h = ogSize[1]
+    n_iter = 5
+
+    xSpeed = (w - croppedSize[0])/n_iter
+    ySpeed = (h - croppedSize[1])/n_iter
+    curr_clip = clip
+
+    for i in range(n_iter):
+        curr_clip = zoomFrame(
+            curr_clip,
+            ogSize,
+            (w - i*xSpeed, h - i*ySpeed),
+            center
+        )
+
+        yield curr_clip
+
 clip1 = mpy.ImageClip("hollow knight.png").set_duration(2)
 size = clip1.size
 
@@ -31,5 +50,6 @@ for i in range(5):
         )
     )
 
-video = mpy.concatenate_videoclips(clips, method="compose")
+video = mpy.concatenate_videoclips(list(zoomIn(clip1,ogSize,croppedSize,center)), method="compose")
+# video = mpy.concatenate_videoclips(clips, method="compose")
 video.write_videofile('test.mp4', fps=24)
