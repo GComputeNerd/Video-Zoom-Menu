98767744187b0734b26c3fe4537502d0b14b7a1d
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-08 23:29:22 +0800

    Zooming now works

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from moviepy.editor import *
+import moviepy.editor as mpy
+from moviepy.video.fx.all import crop, resize
 
-clip = ImageClip("hollow knight.png").set_duration(5)
 
-video = concatenate_videoclips([clip], method="compose")
+clip1 = mpy.ImageClip("hollow knight.png").set_duration(5)
+(w,h) = clip1.size
+
+clip2 = crop(clip1,width = w*0.3, height = h*0.3, x_center=w/2, y_center=h/2)
+clip2 = clip2.resize(width = w)
+
+video = mpy.concatenate_videoclips([clip1, clip2], method="compose")
 video.write_videofile('test.mp4', fps=24)
