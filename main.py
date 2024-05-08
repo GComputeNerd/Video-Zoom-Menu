d0cda41aea2e5d249d73cbbb5ec00d6380392e26
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-08 23:42:13 +0800

    Zoom in kind of works

--- main.py
+++ main.py
@@ -9,10 +9,27 @@ def zoomFrame(clip, result,cropped, center):
     
     return n_clip.resize(result)
 
-clip1 = mpy.ImageClip("hollow knight.png").set_duration(5)
+clip1 = mpy.ImageClip("hollow knight.png").set_duration(2)
 size = clip1.size
 
 clip2 = zoomFrame(clip1, size, (320, 320), (size[0]/2, size[1]/2))
 
-video = mpy.concatenate_videoclips([clip1, clip2], method="compose")
+ogSize = clip1.size
+croppedSize = (320, 320)
+center = (ogSize[0]/2, ogSize[1]/2)
+clips = [clip1]
+n_iter = 5
+xSpeed = (ogSize[0] - croppedSize[0])/n_iter
+ySpeed = (ogSize[1] - croppedSize[1])/n_iter
+for i in range(5):
+    clips.append(
+        zoomFrame(
+            clips[-1],
+            ogSize,
+            (ogSize[0] - i*xSpeed, ogSize[1] - i*ySpeed),
+            center
+        )
+    )
+
+video = mpy.concatenate_videoclips(clips, method="compose")
 video.write_videofile('test.mp4', fps=24)
