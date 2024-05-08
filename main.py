ffdaab442c6277cf5fec7bc2293f6639fb4d91d7
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-08 23:33:58 +0800

    Made zoomFrame function

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import moviepy.editor as mpy
 from moviepy.video.fx.all import crop, resize
 
+def zoomFrame(clip, result,cropped, center):
+    n_clip = clip.crop(width=cropped[0],
+                       height=cropped[1],
+                       x_center = center[0],
+                       y_center = center[1])
+    
+    return n_clip.resize(result)
 
 clip1 = mpy.ImageClip("hollow knight.png").set_duration(5)
-(w,h) = clip1.size
+size = clip1.size
 
-clip2 = crop(clip1,width = w*0.3, height = h*0.3, x_center=w/2, y_center=h/2)
-clip2 = clip2.resize(width = w)
+clip2 = zoomFrame(clip1, size, (320, 320), (size[0]/2, size[1]/2))
 
 video = mpy.concatenate_videoclips([clip1, clip2], method="compose")
 video.write_videofile('test.mp4', fps=24)
