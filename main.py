95ddcea168251f3c2da5aeff95f596f91412cd1a
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-05-09 13:55:04 +0800

    Implemented Zoom Out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import moviepy.editor as mpy
-from moviepy.video.fx.all import crop, resize
+from moviepy.video.fx.all import crop, resize, time_mirror
 
 def zoomFrame(clip, result,cropped, center):
     n_clip = clip.crop(width=cropped[0],
@@ -9,47 +9,54 @@ def zoomFrame(clip, result,cropped, center):
     
     return n_clip.resize(result)
 
-def zoomIn(clip, ogSize, croppedSize, center):
+def zoomIn(clip, ogSize, croppedSize ,center=None, duration=10,
+            initial_duration = 3, final_duration = 2):
     w = ogSize[0]
     h = ogSize[1]
-    n_iter = 5
+    fps = 24
+
+    final = clip.set_duration(initial_duration)
+    clip = clip.set_duration(1/fps)
+
+    n_iter = duration*fps
+
+    if not center:
+        center = (w/2, h/2)
 
     xSpeed = (w - croppedSize[0])/n_iter
     ySpeed = (h - croppedSize[1])/n_iter
     curr_clip = clip
 
     for i in range(n_iter):
+        w -= xSpeed
+        h -= ySpeed
         curr_clip = zoomFrame(
-            curr_clip,
+            clip,
             ogSize,
-            (w - i*xSpeed, h - i*ySpeed),
+            (w, h),
             center
         )
 
-        yield curr_clip
+        final = mpy.concatenate_videoclips([final, curr_clip])
+    final = mpy.concatenate_videoclips([final, curr_clip.set_duration(final_duration)])
 
-clip1 = mpy.ImageClip("hollow knight.png").set_duration(2)
-size = clip1.size
+    return final
 
-clip2 = zoomFrame(clip1, size, (320, 320), (size[0]/2, size[1]/2))
+def zoomOut(clip, ogSize, croppedSize, center=None, duration=3
+    ,       initial_duration = 2, final_duration = 2):
+    total_duration = initial_duration+duration+final_duration
 
+    clip = zoomIn(clip, ogSize, croppedSize, center=None,
+                  initial_duration=final_duration,
+                  final_duration=initial_duration+1,
+                  duration=duration)
+    
+    return time_mirror(clip.subclip(0,total_duration))
+    
+
+
+clip1 = mpy.ImageClip("hollow knight.png").set_duration(1/24)
 ogSize = clip1.size
-croppedSize = (320, 320)
-center = (ogSize[0]/2, ogSize[1]/2)
-clips = [clip1]
-n_iter = 5
-xSpeed = (ogSize[0] - croppedSize[0])/n_iter
-ySpeed = (ogSize[1] - croppedSize[1])/n_iter
-for i in range(5):
-    clips.append(
-        zoomFrame(
-            clips[-1],
-            ogSize,
-            (ogSize[0] - i*xSpeed, ogSize[1] - i*ySpeed),
-            center
-        )
-    )
-
-video = mpy.concatenate_videoclips(list(zoomIn(clip1,ogSize,croppedSize,center)), method="compose")
-# video = mpy.concatenate_videoclips(clips, method="compose")
-video.write_videofile('test.mp4', fps=24)
+w,h = ogSize
+croppedSize = (w*0.3, h*0.3)
+zoomOut(clip1, ogSize, croppedSize).write_videofile("test1.mp4", fps=24)
